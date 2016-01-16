f42d0a1c8f95802566c0f3976117651aee11adb2
migutak <migutak@gmail.com>
2016-01-16 10:41:21 +0000

    mm start

--- rest/add_forward_order.php
+++ rest/add_forward_order.php
@@ -5,39 +5,30 @@
 
 
     $orderid = $_GET['orderid'];
+    $forwardid = $_GET['forwardid'];
     $usernamefk = $_GET['usernamefk'];
     $ccypair = $_GET['ccypair'];
-    $nearbuyorderamountccy = $_GET['nearbuyorderamountccy'];
-    $neardate = $_GET['neardate'];
+    $buyorderamountccy = $_GET['buyorderamountccy'];
+    $buyorderamount = $_GET['buyorderamount'];
+    $sellorderamountccy = $_GET['sellorderamountccy'];
+    $sellorderamount = $_GET['sellorderamount'];
+    $settlementdate = $_GET['settlementdate'];
     $buysell = $_GET['buysell'];
     $buysellbank = $_GET['buysellbank'];
     $recipient = $_GET['recipient'];
-    $fardate = $_GET['fardate'];
     $custcomment = $_GET['custcomment'];
     $ordertypefk = $_GET['ordertypefk'];
-    $nearbuyorderamount = $_GET['nearbuyorderamount'];
-    $nearsellorderamountccy = $_GET['nearsellorderamountccy'];
-    $nearsellorderamount = $_GET['nearsellorderamount'];
-    $farbuyorderamountccy = $_GET['farbuyorderamountccy'];
-    $farbuyorderamount = $_GET['farbuyorderamount'];
-    $farsellorderamountccy = $_GET['farsellorderamountccy'];
-    $farsellorderamount = $_GET['farsellorderamount'];
-    
-    $neardate = $_GET['neardate'];
-    $buysell = $_GET['buysell'];
-    $buysellbank = $_GET['buysellbank'];
-    $recipient = $_GET['recipient'];
-    $fardate = $_GET['fardate'];
+
 
 // array for JSON response
         $response = array();
 
-		$addorder = "insert into Swaporders(orderid,usernamefk,ccypair,nearbuyorderamountccy,nearbuyorderamount,nearsellorderamountccy,nearsellorderamount,buysell,buysellbank,recipient,neardate,fardate,farbuyorderamountccy,farbuyorderamount,farsellorderamountccy,farsellorderamount,custcomment,ordertypefk,currentstatus) 
-                                    values ('$orderid','$usernamefk','$ccypair','$nearbuyorderamountccy','$nearbuyorderamount','$nearsellorderamountccy','$nearsellorderamount','$buysell','$buysellbank','$recipient','$neardate','$fardate','$farbuyorderamountccy','$farbuyorderamount','$farsellorderamountccy','$farsellorderamount','$custcomment','$ordertypefk','N')";
+		$addorder = "insert into Forwardorders(orderid,forwardid,usernamefk,ccypair,buyorderamountccy,buyorderamount,sellorderamountccy,sellorderamount,settlementdate,buysell,buysellbank,recipient,custcomment,ordertypefk) 
+                                    values ('$orderid','$forwardid','$usernamefk','$ccypair','$buyorderamountccy','$buyorderamount','$sellorderamountccy','$sellorderamount','$settlementdate','$buysell','$buysellbank','$recipient','$custcomment','$ordertypefk')";
            
 
         if($db->query($addorder) === TRUE){
-        $response["result"] = "FxSwap Order Created";
+        $response["result"] = "FxForward Order Created";
         }
         else{
             $response["result"] = "Error: " . $addorder . "<br>" . $db->error;
---
+++ rest/add_mm.php
@@ -0,0 +1,36 @@
+<?php 
+	require_once('connect.php');
+	$postdata = file_get_contents("php://input");
+    $request = json_decode($postdata);
+
+
+    $orderid = $_GET['orderid'];
+    $usernamefk = $_GET['usernamefk'];
+    $ccy = $_GET['ccy'];
+    $orderamount = $_GET['orderamount'];
+    $mmfrom = $_GET['mmfrom'];
+    $mmto = $_GET['mmto'];
+    $tenure = $_GET['tenure'];
+    $recipient = $_GET['recipient'];
+    $settlementdate = $_GET['settlementdate'];
+    $custcomment = $_GET['custcomment'];
+    $ordertypefk = $_GET['ordertypefk'];
+
+// array for JSON response
+        $response = array();
+
+		$addorder = "insert into Moneymarketorders(orderid,usernamefk,ccy,orderamount,mmfrom,mmto,tenuredays,recipient,custcomment,ordertypefk) 
+            values ('$orderid','$usernamefk','$ccy','$orderamount','$mmfrom','$mmto','$tenure','$recipient','$custcomment','$ordertypefk')";
+           
+
+        if($db->query($addorder) === TRUE){
+        $response["result"] = "Money Market Order Created and sent";
+        }
+        else{
+            $response["result"] = "Error: " . $addorder . "<br>" . $db->error;
+        }
+	
+	//$db->close();
+	header('Content-type: text/plain');
+	echo json_encode($response);
+?>
